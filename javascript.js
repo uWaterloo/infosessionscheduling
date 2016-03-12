68f5bb1e6ab16bba8227d2354c686414d1a17181
kxdang <ikien.dang@gmail.com>
2016-03-12 11:49:25 -0500

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -1,73 +1,90 @@
 angular.module('portalApp')
 
 // Widget controller - runs every time widget is shown
-.controller('infosessionschedulingCtrl', ['$scope', '$http', '$q', 'infosessionschedulingFactory', function ($scope, $http, $q, infosessionschedulingFactory) {
+.controller('infosessionschedulingCtrl', ['$scope', '$http', '$q', 'infosessionschedulingFactory', function($scope, $http, $q, infosessionschedulingFactory) {
 
-    // Widget Configuration
-    $scope.portalHelpers.config = {
-        // make 'widgetMenu.html' the template for the top right menu
-        "widgetMenu": "widgetMenu.html"
-    };
+        // Widget Configuration
+        $scope.portalHelpers.config = {
+            // make 'widgetMenu.html' the template for the top right menu
+            "widgetMenu": "widgetMenu.html"
+        };
 
-    // Import variables and functions from service
-    $scope.data = infosessionschedulingFactory.data;
+        // Import variables and functions from service
 
-    // initialize the service
-    infosessionschedulingFactory.init($scope);
+        // initialize the service
+        infosessionschedulingFactory.init($scope);
 
-	// Show main view in the first column
-	$scope.portalHelpers.showView('main.html', 1);
-    
-    var url = 'https://api.uwaterloo.ca/v2/terms/1163/infosessions.json?key=36802f2c7eab5943ece0bcf8eec07d5a';
-    // http.get FUNCTION 
-	$http.get(url).success(function(result) { 
-		// Handle result 
-		console.log(result);
-        $scope.mydata = result.data;
-	}); 
-// http.get FUNCTION 
+        // Show main view in the first column
+        $scope.portalHelpers.showView('main.html', 1);
 
-	
-}])
-// Factory maintains the state of the widget
-.factory('infosessionschedulingFactory', ['$http', '$rootScope', '$filter', '$q', function ($http, $rootScope, $filter, $q) {
-		
-	var initialized = {value: false};
+        var url = 'https://api.uwaterloo.ca/v2/terms/1163/infosessions.json?key=36802f2c7eab5943ece0bcf8eec07d5a';
+        // http.get FUNCTION 
+        $http.get(url).success(function(result) {
+            // Handle result 
+            $scope.mydata = result.data;
+        });
+        // http.get FUNCTION 
+        //Todays date
+        var currentTime = new Date();
+    	var month=currentTime.getMonth()+1;
+    	var year=currentTime.getFullYear();
+    	var day=currentTime.getDate();
+    	if (day<10){
+            day='0'+day;
+        }
+    	if (month<10){
+            month='0'+month;
+        }
+  
+    	var totaldate=year+"-"+month+"-"+day
+		console.log(totaldate);
+    	
+    }])
+    // Factory maintains the state of the widget
+    .factory('infosessionschedulingFactory', ['$http', '$rootScope', '$filter', '$q', function($http, $rootScope, $filter, $q) {
 
-	// Your variable declarations
-	var data = {value: null};
+        var initialized = {
+            value: false
+        };
 
-	var init = function ($scope) {
-		if (initialized.value)
-			return;
-		
-		initialized.value = true;
+        // Your variable declarations
+        var data = {
+            value: null
+        };
 
-		// Place your init code here:
-		data.value={message:"Welcome to Portal SDK!"};
-	}
+        var init = function($scope) {
+            if (initialized.value)
+                return;
+
+            initialized.value = true;
+
+            // Place your init code here:
+            data.value = {
+                message: "Welcome to Portal SDK!"
+            };
+        }
 
 
-	// Expose init(), and variables
-	return {
-		init: init,
-		data: data
-	};
+        // Expose init(), and variables
+        return {
+            init: init,
+            data: data
+        };
 
-}])
-// Custom directive example
-.directive('infosessionschedulingDirectiveName', ['$http', function ($http) {
-	return {
-		link: function (scope, el, attrs) {
+    }])
+    // Custom directive example
+    .directive('infosessionschedulingDirectiveName', ['$http', function($http) {
+        return {
+            link: function(scope, el, attrs) {
 
-		}
-	};
-}])
-// Custom filter example
-.filter('infosessionschedulingFilterName', function () {
-	return function (input, arg1, arg2) {
-		// Filter your output here by iterating over input elements
-		var output = input;
-		return output;
-	}
-});
+            }
+        };
+    }])
+    // Custom filter example
+    .filter('infosessionschedulingFilterName', function() {
+        return function(input, arg1, arg2) {
+            // Filter your output here by iterating over input elements
+            var output = input;
+            return output;
+        }
+    });
